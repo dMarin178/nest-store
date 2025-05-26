e3270c94480ffca6a47f5085d07917b5c22cce01
Dino Marin Diaz <dinomarindiaz178@gmail.com>
2025-05-25 23:32:27 -0400

    .env validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { HttpModule, HttpService} from '@nestjs/axios';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { UsersModule } from './users/users.module';
 import { lastValueFrom } from 'rxjs';
 import { DatabaseModule } from './database/database.module';
-import { ConfigModule } from '@nestjs/config';
 import { enviroments } from './enviroments';
 import config from './config';
 
@@ -16,6 +18,15 @@ import config from './config';
       envFilePath: enviroments[process.env.NODE_ENV] || '.env',//ENV VARIABLES
       load: [config],//CONFIGURATION FILE
       isGlobal: true,//USE IN ALL MODULES
+      validationSchema: Joi.object({
+        DATABASE_NAME: Joi.string().required(),
+        DATABASE_HOST: Joi.string().default('localhost'),
+        DATABASE_PORT: Joi.number().default(5432),
+        DATABASE_USER: Joi.string().required(),
+        DATABASE_PASSWORD: Joi.string().required(),
+        API_KEY: Joi.string().required(),
+        NODE_ENV: Joi.string().valid('dev', 'prod', 'test').default('dev'),
+      })
     }),
     DatabaseModule,
     HttpModule,
