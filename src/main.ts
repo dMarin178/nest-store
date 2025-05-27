6de539f088940d7fc3bad6848a7c03e04f3ab6bf
Dino Marin Diaz <dinomarindiaz178@gmail.com>
2025-05-26 22:12:34 -0400

    adding cors

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,15 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
 
+  //CORS CONFIGURATION
+  app.enableCors({
+    origin: 'http://localhost:3001',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    // NestJS no admite `preflightContinue` aquí, lo maneja automáticamente.
+  });
+
+  //GLOBAL PIPES CONFIGURATION
   app.useGlobalPipes(new ValidationPipe({
     //whitelist true, //Ignora los atributos que no esten en el dto
     whitelist: true,
